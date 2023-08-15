8b73f689ecb6f707666cbbde22427cb99c76e7e2
Beer Koala <beer_koala@icloud.com>
2023-08-15 18:03:18 +0200

    Initial Commit

---
+++ Quoteslist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quoteslist
+//
+//  Created by Beer Koala on 15.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
